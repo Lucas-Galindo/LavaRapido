b6452f43a7fae0cc13333cf17caecbf39022e62b
Lucas Galindo <srlucasmtg@gmail.com>
2025-07-02 23:24:07 -0300

    First commit

---
+++ src/main/java/com/example/app_lavarapido/AppLavarapidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_lavarapido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLavarapidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLavarapidoApplication.class, args);
+    }
+
+}
